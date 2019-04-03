a5cad1bc36d9c1345ad0f7bf68b96411ff384a0d
Ralf Beckers <ralf.beckers@elektrobit.com>
2019-04-03 12:40:08 +0200

    Add demox

---
+++ WpfDeviceCatcherMvvmCore/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using UsbMonitor;
+
+namespace DeviceCatcherMvvm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : UsbMonitorWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
